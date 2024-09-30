8b7250ed4c14764d1254819be5f5e964485e5708
jchrishammond <jchrisha@gmail.com>
2024-09-30 17:38:42 +0000

    User exercise (easy)

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.example.tourservice.TourManagementService;
 import com.example.tourservice.TravelAgentService;
 
 public class Application {
@@ -11,7 +12,10 @@ public class Application {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+        TourManagementService svc = context.getBean(TourManagementService.class);
+
         System.out.println("\n******Explore California Tour Catalogue******");
+        svc.createTour("Zoo Tour", 100, true);
         agent.displayTours();
 
         System.out.println("\n******Explore California Tour Kid Friendly Tours******");
